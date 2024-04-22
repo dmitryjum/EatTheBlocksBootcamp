3819fde99fce907a1abdfbd00f940fb12dd8107a
Dmitry Jum <xfiler@ymail.com>
2024-04-22 04:30:49 -0700

    tests almost everything in the Charity contract

--- homework_4/test/Charity.t.sol
+++ homework_4/test/Charity.t.sol
@@ -9,6 +9,8 @@ contract CharityTest is Test {
   address owner = address(1);
   address donator;
   uint256 donationAmount;
+  event Donated(address indexed donator, uint256 amount);
+  event Withdrawn(uint256 amount);
   
   mapping(address => uint256) public userDonations;
 
@@ -30,11 +32,23 @@ contract CharityTest is Test {
     donationAmount = 10 ether;
     charity = new Charity(owner, 2 days);
     hoax(donator, donationAmount);
+    vm.expectEmit(true, true, true, true);
+    emit Donated(donator, donationAmount);
     charity.donate{value: donationAmount}();
     uint256 currentBalance = address(charity).balance;
 
     assertEq(currentBalance, donationAmount);
     assertEq(charity.userDonations(donator), donationAmount);
+  }
+
+  function test_withdraw() public {
+    charity = new Charity(owner, 2 days);
+    vm.prank(owner);
+    deal(address(charity), 4 ether);
+    vm.expectEmit(true, true, true, true);
+    emit Withdrawn(4 ether);
+    charity.withdraw(4 ether);
+    assertEq(address(charity).balance, 0 ether);
   }
 
   function test_RevertWhen_CanNotDonateAnymore() public {
@@ -51,6 +65,20 @@ contract CharityTest is Test {
     vm.expectRevert(abi.encodeWithSelector(Charity.NotEnoughDonationAmount.selector));
     hoax(donator, donationAmount);
     charity.donate{value: donationAmount}();
+  }
+
+  function test_RevertNotOwner() public {
+    charity = new Charity(owner, 2 days);
+    vm.expectRevert(abi.encodeWithSelector(Charity.NotOwner.selector));
+    charity.withdraw(5 ether);
+  }
+
+  function test_RevertNotEnoughMoney() public {
+    charity = new Charity(owner, 2 days);
+    vm.expectRevert(abi.encodeWithSelector(Charity.NotEnoughMoney.selector));
+    vm.prank(owner);
+    deal(address(charity), 4 ether);
+    charity.withdraw(5 ether);
   }
   
 }
