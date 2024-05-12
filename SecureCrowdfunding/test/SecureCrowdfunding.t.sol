2021e9a4fd4583ee2b33aad92abb8a62cb27ec82
Dmitry Jum <xfiler@ymail.com>
2024-05-11 20:15:31 -0700

    sets up crowdfunding exercise to test and refactor with slither and tests

---
+++ SecureCrowdfunding/test/SecureCrowdfunding.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.25;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SecureCrowdfunding} from "../src/SecureCrowdfunding.sol";
+
+contract SecureCrowdfundingTest is Test {
+
+}
